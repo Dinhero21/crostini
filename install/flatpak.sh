b7f39c7721566b51c403b3c57819304a3ee462d8
Dinhero21 <78858626+Dinhero21@users.noreply.github.com>
2024-01-24 03:01:32 -0300

    Fixed flatpak user permissions

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -4,4 +4,4 @@ sudo apt update
 
 sudo apt install flatpak -y
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
